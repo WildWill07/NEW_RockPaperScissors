eabf38bac2e4003a170e8272c0dc7d9bd1c5eac4
WildWill07 <100398202+WildWill07@users.noreply.github.com>
2023-09-18 21:16:27 -0500

    Added internal documentation for functions. Wrote initial logic of the evaluate() function. Added todo list to README.md

--- README.md
+++ README.md
@@ -16,3 +16,6 @@
 2. getPlayerSelection() - gets players selection; takes no parameters; returns an integer
 3. evaluate() - evluates the winner of the round & increments score variables accordingly; takes two paramters playerSelection & computerSelection; returns a string notifying the winner.
 4. play() - recursive function for looping.
+
+# TODO
+1. Investigate to see if the evaulate() function can be refactored into more concise code.
--- main.js
+++ main.js
@@ -5,12 +5,21 @@ let computerSelection = 0;
 
 let controlVar = 0;
 
+//PROGRAM ENTRANCE
+while (controlVar < 5) {
+    play();
+    controlVar++;
+}
+
+//Calls required functions to get computer selection & player selection & stores returned values
+//Returned values are then sent to be evaluated
 function play() {
     playerSelection = getPlayerSelection();
     computerSelection = getComputerSelection();
     evaluate(playerSelection, computerSelection)
 }
 
+//Promots user for input and returns it play()
 function getPlayerSelection() {
     return prompt("Please enter the corresponding integer to your selection:\
     1 - Rock\
@@ -18,15 +27,50 @@ function getPlayerSelection() {
     3 - Paper");
 }
 
+//Picks a random number 1-3 for computer seleciton & returns it to play()
 function getComputerSelection() {
     return Math.floor(Math.random() * (4 - 1) + 1);
 }
 
+//Takes in playerSelection & computerSelection and evaluates a winner
 function evaluate(playerSelection, computerSelection) {
-    console.log("The player selected: " + playerSelection + " The computer selected: "+ computerSelection);
-}
+    switch(playerSelection) {
+        case 1:
+            if(computerSelection==2) {
+                //player win
+            }
+            else if(computerSelection==3) {
+                //computer win
+            }
+            else {
+                //draw
+            }
+            break;
+        case 2:
+            if(computerSelection==3) {
+                //player win
+            }
+            else if(computerSelection==1) {
+                //computer win
+            }
+            else {
+                //draw
+            }
+            break;
+        case 3:
+            if(computerSelection==1) {
+                //player win
+            }
+            else if(computerSelection==2) {
+                //computer win
+            }
+            else {
+                //draw
+            }
+            break;
+        default:
+            console.log("Something has gone wrong in the switch statement.")
+    }
 
-while (controlVar < 5) {
-    play();
-    controlVar++;
+    console.log("The player selected: " + playerSelection + " The computer selected: "+ computerSelection);
 }
